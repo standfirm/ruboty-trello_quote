fc14cd83f90728fbd1bc8466c62998598ea8e569
mzp <mzpppp@gmail.com>
2015-04-24 16:35:50 +0900

    generate file

---
+++ lib/ruboty/trello_quote.rb
@@ -0,0 +1,7 @@
+require "ruboty/trello_quote/version"
+
+module Ruboty
+  module TrelloQuote
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/trello_quote/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module TrelloQuote
+    VERSION = "0.0.1"
+  end
+end
